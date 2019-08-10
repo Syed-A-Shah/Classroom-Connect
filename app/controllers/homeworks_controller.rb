672d9393aa5b01f04050697d33048afb68b77eaf
Syed-A-Shah <ssyed1559@gmail.com>
2019-08-10 05:12:48 +0100

    before_action added with user's email shows with the post

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,18 +1,23 @@
 class HomeworksController < ApplicationController
-    def index
-        @homeworks = Homework.all
-    end
-    def new
-        @homework = current_user.homeworks.build
-    end
-    def create
-        @homework = current_user.homeworks.create(post_params)
-        redirect_to '/homeworks'
-      end
-    private
+  before_action :authenticate_user!
 
-    def post_params
-        params.require(:homework).permit(:hw, :caption,)
-    end
+  def index
+    @homeworks = Homework.all
+  end
+
+  def new
+    @homework = current_user.homeworks.build
+  end
+
+  def create
+    @homework = current_user.homeworks.create(post_params)
+    redirect_to '/homeworks'
+  end
+  
+  private
+
+  def post_params
+    params.require(:homework).permit(:hw, :caption,)
+  end
 
 end
